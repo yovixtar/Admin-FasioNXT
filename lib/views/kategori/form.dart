b6363c169a2587ca3ec0274f265410bc7ff8c123
KhazimStar <khazim.fikri.9f@gmail.com>
2024-07-21 11:52:29 +0700

    Feature : Kategori CRUD

---
+++ lib/views/kategori/form.dart
@@ -0,0 +1,105 @@
+import 'package:admin_fasionxt/services/ApiKategori.dart';
+import 'package:admin_fasionxt/views/colors.dart';
+import 'package:admin_fasionxt/views/layout_menu.dart';
+import 'package:admin_fasionxt/views/utils/snackbar_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:admin_fasionxt/models/kategori.dart';
+
+class FormKategori extends StatefulWidget {
+  final Kategori? kategori;
+
+  FormKategori({this.kategori});
+
+  @override
+  _FormKategoriState createState() => _FormKategoriState();
+}
+
+class _FormKategoriState extends State<FormKategori> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final APIKategoriService _apiService = APIKategoriService();
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.kategori != null) {
+      _namaController.text = widget.kategori!.nama;
+    }
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState!.validate()) return;
+    setState(() {
+      _isLoading = true;
+    });
+    Map<String, dynamic> result;
+    if (widget.kategori == null) {
+      result = await _apiService.create(nama: _namaController.text);
+    } else {
+      result = await _apiService.update(
+          id: widget.kategori!.id, nama: _namaController.text);
+    }
+    setState(() {
+      _isLoading = false;
+    });
+    if (result['success'] != null) {
+      Navigator.pop(context);
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LayoutMenu(
+            toPage: 0,
+          ),
+        ),
+      );
+      SnackbarUtils.showSuccessSnackbar(context, result['success']);
+    } else {
+      SnackbarUtils.showErrorSnackbar(context, result['error']);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            widget.kategori == null ? 'Tambah Kategori' : 'Update Kategori'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _namaController,
+                decoration: InputDecoration(labelText: 'Nama Kategori'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama kategori tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: purplePrimary,
+                      ),
+                      onPressed: _submit,
+                      child: Text(
+                        widget.kategori == null
+                            ? 'Tambah Kategori'
+                            : 'Update Kategori',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/layout_menu.dart
@@ -0,0 +1,91 @@
+import 'package:admin_fasionxt/views/colors.dart';
+import 'package:admin_fasionxt/views/kategori/list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LayoutMenu extends StatefulWidget {
+  final int toPage;
+
+  const LayoutMenu({super.key, this.toPage = 0});
+
+  @override
+  State<LayoutMenu> createState() => _LayoutMenuState();
+}
+
+class _LayoutMenuState extends State<LayoutMenu> {
+  int currentIndex = 0;
+  List<Widget> screens = [
+    DaftarKategori(),
+    Scaffold(),
+    Scaffold(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.toPage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: _buildNavBar(),
+      body: screens[currentIndex],
+    );
+  }
+
+  Widget _buildNavBar() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: ClipRRect(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildNavItem(Icons.home, 0),
+            _buildNavItem(Icons.notifications, 1),
+            _buildNavItem(Icons.shopping_cart, 2),
+            _buildNavItem(Icons.person, 3),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: (currentIndex == index)
+                  ? EdgeInsets.all(8)
+                  : EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  color: currentIndex == index
+                      ? Colors.white
+                      : const Color.fromARGB(0, 0, 0, 0),
+                  shape: BoxShape.circle),
+              child: Icon(
+                icon,
+                size: (currentIndex == index) ? 35 : 30,
+                color: currentIndex == index ? ancientRed : Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
